5d73c5cbf951b72467afe6aa38d746d6f72c8a8a
louiseconlon11 <louiseconlon11@gmail.com>
2023-02-24 15:41:30 +0000

    update code for subtract and division game to include ternary operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,8 +87,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 }
 
@@ -99,7 +99,7 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operator").textContent = "/";
 }
