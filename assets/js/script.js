6b59c98957ec598a25722135a9f097fdc1185b17
louiseconlon11 <louiseconlon11@gmail.com>
2023-02-24 13:51:55 +0000

    add details to the calculate correct answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,16 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator");
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`unimplemented operator ${operator}`);
+        throw `unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
